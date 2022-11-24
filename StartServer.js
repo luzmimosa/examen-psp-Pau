fb65627938d2fe9178dc0bf1a7ab98365f378421
Pau Salanova Blanco <pauete.sb@gmail.com>
2022-11-24 04:05:44 +0100

    the server now handles css and images requests

--- StartServer.js
+++ StartServer.js
@@ -11,6 +11,7 @@ database.connect();
 const homeRouter = require('./routes/homeRouter');
 const articlesRouter = require('./routes/articlesRouter');
 const clientsRouter = require('./routes/clientsRouter');
+const fs = require("fs");
 
 // Setup app
 const app = express();
@@ -20,6 +21,33 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/'), homeRouter);
 app.use(express.static(__dirname + '/articulos'), articlesRouter);
 app.use(express.static(__dirname + '/clientes'), clientsRouter);
+
+app.get('/images/', (req, res) => {
+    res.redirect('/images/ñ');
+})
+
+app.get('/images/:name', async (req, res) => {
+    let path = __dirname + '/public/images/' + req.params.name
+    console.log("Params: (" + req.params.name + ") Path: (" + path + ")");
+
+    // if the image exists, send it. Otherwise, send the default image (default.png)
+    if (fs.existsSync(path) && req.params.name !== '') {
+        res.sendFile(path);
+        return;
+    }
+    res.sendFile(__dirname + '/public/images/default/default.gif');
+})
+
+app.get('/css/:name', async (req, res) => {
+    let path = __dirname + '/public/css/' + req.params.name
+
+    //if the css exists, send it. Otherwise, send an error
+    if (fs.existsSync(path)) {
+        res.sendFile(path);
+    } else {
+        res.status(404).send('Not found');
+    }
+})
 
 // Start server
 app.listen(3000, () => {
